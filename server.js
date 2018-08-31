3ff3d4bd18e41e8407ff421c95a426260fe869cf
Firmansyah Nuralif Rohman <mendoanjoe@gmail.com>
2018-08-31 10:04:04 +0700

    add http logger & create line service

--- server.js
+++ server.js
@@ -2,12 +2,36 @@
 * Module Loads
 */
 const app = require('express')();
+const line = require('@line/bot-sdk');
+const logger = require('morgan');
 
 /*
-* Route End Point
+* Http Logger
+*/
+app.use(logger('combined'));
+
+/*
+* Configs
+* Url : https://developers.line.me/console/
+*/
+const channelAccessToken = process.env.CHANNEL_ACCESS_TOKEN || "YOUR CHANNEL ACCESS TOKEN";
+const channelSecret = process.env.CHANNEL_SECRET || "YOUR CHANNEL SECRET";
+const config = {
+  'channelAccessToken': channelAccessToken,
+  'channelSecret': channelSecret,
+};
+
+/*
+* Create Line Service
+*/
+const client = new line.Client(config);
+
+/*
+* Route Root End Point
+* Access on : host:port/
 */
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+  res.send('Hello World!');
 });
 
 /*
@@ -15,5 +39,5 @@ app.get('/', (req, res) => {
 */
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
-  console.log('Example app listening on port 3000!')
+  console.log(`Server running on port: ${port}`)
 });
